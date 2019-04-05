68f26aece6b339cb0e8652123fbbe89684bd0bc7
Bohdan Sushchak <bogdansushak@gmail.com>
2019-04-06 00:08:04 +0200

    Fixed design of order item

--- lib/ui/shop_list/order_item.dart
+++ lib/ui/shop_list/order_item.dart
@@ -6,6 +6,8 @@ class OrderItemWidget extends StatelessWidget {
   final Widget child;
   final Order order;
 
+  final textPadding = EdgeInsets.fromLTRB(40, 5, 40, 5);
+
   OrderItemWidget({Key key, this.order, this.child}) : super(key: key);
 
   @override
@@ -13,14 +15,37 @@ class OrderItemWidget extends StatelessWidget {
     return Center(
       child: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            new Text(order.shopName),
-            new Text(order.date),
-            new Container(
-              color: COLOR_ACCENT,
-              height: 2,
-              width: double.infinity,
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Container(
+                padding: textPadding,
+                child: Text(
+                  order.shopName,
+                  style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold),
+                ),
+              )
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    padding: textPadding,
+                    child: Text(
+                      order.date,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    )),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(25, 5, 25, 10),
+              child: Container(
+                color: COLOR_ACCENT,
+                height: 2,
+                width: double.infinity,
+              ),
             ),
           ],
         ),
