bfdd926389c61ea17d8305a3df109e44bdcb2402
Bohdan Sushchak <bogdansushak@gmail.com>
2019-04-06 00:16:16 +0200

    Order list page add floatingActionButton

--- lib/ui/shop_list/order_list_page.dart
+++ lib/ui/shop_list/order_list_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_list_app/data/model/error_response.dart';
+import 'package:shop_list_app/internal/app_colors.dart';
 import 'package:shop_list_app/ui/shop_list/order_item.dart';
 import 'package:shop_list_app/ui/shop_list/order_list_bloc.dart';
 import 'package:shop_list_app/ui/shop_list/order_list_state.dart';
@@ -20,6 +20,8 @@ class _OrderListPageState extends State<OrderListPage> {
   final _orderListBloc = kiwi.Container().resolve<OrderListBloc>();
   final _scrollController = ScrollController();
 
+  void _goToAddOrderPage() {}
+
   @override
   Widget build(BuildContext context) {
     return _buildScafold();
@@ -27,17 +29,26 @@ class _OrderListPageState extends State<OrderListPage> {
 
   Scaffold _buildScafold() {
     return new Scaffold(
-        body: new DecoratedContainer(
-            child: Center(
-                child: new BlocBuilder(
-                    bloc: _orderListBloc,
-                    builder: (context, OrderListState state) {
-                      if (state.isLoading) return CircularProgressIndicator();
-                      if (state.isSuccessful) {
-                        return _buildOrderList(state);
-                      } else
-                        return Container();
-                    }))));
+      body: new DecoratedContainer(
+          child: Center(
+              child: new BlocBuilder(
+                  bloc: _orderListBloc,
+                  builder: (context, OrderListState state) {
+                    if (state.isLoading) return CircularProgressIndicator();
+                    if (state.isSuccessful) {
+                      return _buildOrderList(state);
+                    } else
+                      return Container();
+                  }))),
+      floatingActionButton: Container(
+        padding: EdgeInsets.only(right: 10, bottom: 20),
+        child: FloatingActionButton(
+          onPressed: _goToAddOrderPage,
+          backgroundColor: COLOR_ACCENT,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
   }
 
   Widget _buildOrderList(OrderListState state) {
